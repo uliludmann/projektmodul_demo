c104474e85b9cf538d2a4871f36b1ce17e5b2c6b
uliludmann <ulrichludmann@gmail.com>
2017-10-24 13:49:20 +0200

    feat: anforderungen erfüllt

---
+++ helpers.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+filename = 'n.txt'
+data = []
+
+def read_file(filename):
+	"""reads a txt file. Format = 1 row - 1 int
+	returns array with all ints"""
+	data = []
+	with open(filename) as f:
+		for l in f.readlines():
+			data.append(int(l.rstrip()))
+	return data
+
+def calculate_sum(data):
+	"""arg: int-array
+	return sum of integer array
+	"""
+	sum_ = 0
+	for i in data:
+		sum_ += i
+	return sum_
---
+++ main.py
@@ -0,0 +1,11 @@
+import helpers
+
+a = 5
+b = 10
+
+data = helpers.read_file('n.txt')
+
+sum = helpers.calculate_sum(data)
+print(data)	
+print(sum)
+
